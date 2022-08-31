5df3a739e889432a3611bd92c59c7852adff9e3d
Léo SAMUEL <leosamuel64@gmail.com>
2022-08-31 23:41:21 +0200

    dodo

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ from flask_session import Session
 import os 
 import random
 from datetime import datetime
-import pickle
 
 # Configuration de Flask
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-
-# ----------------------- FONCTIONS AUXILIAIRES -----------------------
-
 
 
 # ----------------------- VARIABLES GLOBALES -----------------------
